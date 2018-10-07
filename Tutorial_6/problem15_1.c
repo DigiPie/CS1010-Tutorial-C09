27141cc208443a44ce02b112d5e0f5b22f3e7816
Evan Tay <e0177031@u.nus.edu>
2018-10-08 01:55:13 +0800

    Added comments to problem15_1.c

--- Tutorial_6/problem15_1.c
+++ Tutorial_6/problem15_1.c
@@ -10,11 +10,16 @@
 double avg(const long list[], long length)
 {
   long sum = list[0];
+  // This is better than:
+  // long sum = 0;
+  // Then subsequently doing a for loop starting with i = 0
+  // As this avoids a wasted assignment of sum = 0, and one loop iteration
   for (long i = 1; i < length; i += 1) {
     sum += list[i];
   }
 
   return sum*1.0/length;
+  // You can also type-cast with (double) to prevent integer division
 }
 
 int main()
